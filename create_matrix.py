2e722b54ffdca7e9e8f438a38c28aeaf6ec3a033
Mohamed Sakr <3dsakr@gmail.com>
2025-12-15 00:50:32 +0200

    c312cu128pyt291

--- create_matrix.py
+++ create_matrix.py
@@ -139,49 +139,49 @@ LINUX_ARM64_SELF_HOSTED_MATRIX = {
 
 WINDOWS_MATRIX = {
     "flash-attn-version": [
-        "2.6.3",
+        # "2.6.3",
         # "2.7.4",
-        # "2.8.3",
+        "2.8.3",
     ],
     "python-version": [
-        "3.10",
-        "3.11",
+        # "3.10",
+        # "3.11",
         "3.12",
         # "3.13",
     ],
     "torch-version": [
-        "2.5.1",
-        "2.6.0",
-        "2.7.1",
-        "2.8.0",
+        # "2.5.1",
+        # "2.6.0",
+        # "2.7.1",
+        # "2.8.0",
         "2.9.1",
     ],
     "cuda-version": [
-        "12.4",
+        # "12.4",
         # "12.6",
         "12.8",
         # "12.9",
-        "13.0",
+        # "13.0",
     ],
 }
 
 WINDOWS_CODEBUILD_MATRIX = {
     "flash-attn-version": [
-        "2.6.3",
-        "2.7.4.post1",
+        # "2.6.3",
+        # "2.7.4.post1",
         "2.8.3",
     ],
     "python-version": [
-        "3.10",
-        "3.11",
+        # "3.10",
+        # "3.11",
         "3.12",
-        "3.13",
+        # "3.13",
     ],
     "torch-version": [
         "2.9.1",
     ],
     "cuda-version": [
-        "13.0",
+        "12.8",
     ],
 }
 
@@ -202,10 +202,10 @@ def main():
                 # "linux_arm64_self_hosted": False,
                 "linux_arm64_self_hosted": LINUX_ARM64_SELF_HOSTED_MATRIX,
                 #
-                "windows": False,
+                "windows": True,
                 # "windows": WINDOWS_MATRIX,
                 #
-                "windows_code_build": False,
+                "windows_code_build": True,
                 # "windows_code_build": WINDOWS_CODEBUILD_MATRIX,
                 #
                 "exclude": EXCLUDE,
